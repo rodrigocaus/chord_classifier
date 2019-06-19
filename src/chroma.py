50c106a47b53208ecfd0d94af1dee50f04c4f9df
Rodrigo Caus <digocaus@hotmail.com>
2019-06-19 14:37:29 -0300

    Alterado um erro que fazia valores de baixa frequência mal avaliados. Implementei uma janela de Hanning em complemento à janela de Gauss e Retangular. Tratanto algumas exceções.

--- src/chroma.py
+++ src/chroma.py
@@ -5,7 +5,7 @@ from scipy import signal
 import math
 
 
-def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='flat'):
+def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='ret'):
     '''
             Calculates the chromagram of an audio sample.
 
@@ -13,8 +13,8 @@ def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='flat'):
                         data: array of time series of an audio sample.
                         rate: sample frequency of 'data'.
                         wilen: lenght of segment of FFT.
-						scale: type of chroma-scale (sharp, flat or number)
-                        winn: type of window (flat, gauss)
+                                                scale: type of chroma-scale (sharp, flat or number)
+                        winn: type of window (ret, gauss, hann)
 
                 Returns:
                         chromas: array of chroma-scale (C, C#, D, ..., B).
@@ -33,7 +33,7 @@ def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='flat'):
         # First frequency is an A at 27.5Hz
         center = 27.5
         # Deviation between chromas
-        devfreq = 0.028
+        devfreq = 0.028060
         chromaidx = 9
         centeridx = 0
 
@@ -43,16 +43,26 @@ def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='flat'):
             upidx = center*(1.0 + devfreq)
             upidx = math.floor((len(f) - 1)*(upidx - f[0])/(fmax - f[0]))
             winfilter = np.zeros(shape=spectrum.shape)
-            
+
             if winn == 'gauss':
                 # Gaussian window inst
                 sigma = (upidx - loidx + 1.0)/6.0
                 mu = (len(f) - 1)*(center - f[0])/(fmax - f[0])
-
                 for i in range(upidx - loidx + 1):
                     winfilter[loidx+i] = __gaussian(loidx + i, mu, sigma)
+            elif winn == 'hann':
+                h = np.hanning(upidx - loidx + 1)
+                winfilter[loidx:upidx+1] = h[0:upidx-loidx+1]
+            elif winn == 'ret':
+                winfilter[loidx:upidx+1] = 1.0
+
+            # Throwing errors on winn argument
+            elif isinstance(winn, str):
+                raise ValueError(
+                    'window format \'{}\' is not implemented.'. format(winn))
             else:
-                winfilter[loidx:upidx] = 1.0
+                raise TypeError(
+                    'a string was expected. Received: {}'. format(type(winn)))
 
             chromagram[chromaidx, timeidx] += np.dot(spectrum, winfilter)
             chromaidx = (chromaidx + 1) % 12
@@ -68,8 +78,16 @@ def chromagram_stft(data, rate=1.0, winlen=2048, scale='sharp', winn='flat'):
         elif scale == 'flat':
             chromas = np.array(['C', 'Db', 'D', 'Eb', 'E', 'F',
                                 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'])
-        else:
+        elif scale == 'number':
             chromas = np.array([i for i in range(12)])
+
+        # Throwing errors on winn argument
+        elif isinstance(scale, str):
+            raise ValueError(
+                'scale format \'{}\' is not implemented.'. format(scale))
+        else:
+            raise TypeError(
+                'a string was expected. Received: {}'. format(type(scale)))
 
     return chromas, t, chromagram
 
@@ -80,6 +98,7 @@ def chromaplot(t, scale, chroma):
     plt.yticks(
         ticks=[i for i in range(len(scale))], labels=scale)
     return ax
+
 
 def __gaussian(x, mu, sig):
     return (1.0/(sig*np.sqrt(2.0*np.pi)))*np.exp(((x-mu)/sig)**2/(-2.0))
